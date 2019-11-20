71ce6abd1661e245c3fdf26200e3c535fb036ca5
Gearóid Reilly <g00332821@gmit.ie>
2019-11-20 10:37:06 +0000

    Team and job are now selected using selects.

--- src/app/create-robot/create-robot.component.ts
+++ src/app/create-robot/create-robot.component.ts
@@ -9,6 +9,37 @@ import {RobotDBService} from '../services/robot-db.service';
 })
 export class CreateRobotComponent implements OnInit {
 
+  selectedJob: string;      //Get selected Job value
+  selectedTeam: string;     //Get selected Team value
+
+  //Array of available jobs
+  jobs: string[] = [
+    "Warrior",
+    "Rogue",
+    "Cleric",
+    "Mage",
+    "Monk",
+    "Paladin",
+    "Bard",
+    "Berserker"
+  ];
+
+  //Array of available teams
+  teams: string[] = [
+    "Aries",
+    "Taurus",
+    "Gemini",
+    "Cancer",
+    "Leo",
+    "Virgo",
+    "Libra",
+    "Scorpio",
+    "Sagittarius",
+    "Capricorn",
+    "Aquarius",
+    "Pisces"
+  ]
+
   constructor(private roboRoute: RobotDBService) { }
 
   ngOnInit() {
@@ -17,11 +48,7 @@ export class CreateRobotComponent implements OnInit {
   //Function to add a robot, accessed from the form
   AddRobot(form: NgForm){
     //Access the add robot function of the RobotDBService and pass the form values in
-    this.roboRoute.AddRobot(form.value.name, form.value.job, form.value.team).subscribe();
-
-    //Log the results to test results
-    console.log(form.value);
-    console.log(form.value.name);
+    this.roboRoute.AddRobot(form.value.name, this.selectedJob, this.selectedTeam).subscribe();
 
     //Reset the form
     form.resetForm();
