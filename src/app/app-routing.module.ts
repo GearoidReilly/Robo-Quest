ff88f516b4d5283885ee0e07db03967a9d223e89
Gearóid Reilly <g00332821@gmit.ie>
2019-11-24 23:09:50 +0000

    Added Routing path to home page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CreateRobotComponent} from './create-robot/create-robot.component';
 import {ReadRobotComponent} from './read-robot/read-robot.component';
-import { EditRobotComponent} from './edit-robot/edit-robot.component'
+import { EditRobotComponent} from './edit-robot/edit-robot.component';
+import { HomePageComponent} from './home-page/home-page.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent
+  },
   {
     path: 'read',
     component: ReadRobotComponent
